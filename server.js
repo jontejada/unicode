d66096affd1d616fcbe41217dd9bed3efc53c2e3
Jon Tejada <tejjon@gmail.com>
2015-12-09 15:57:30 -0800

    static change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
