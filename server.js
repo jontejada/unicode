e39cac4ab32c477ffd8933a8e98a741f79138948
Jon Tejada <tejjon@gmail.com>
2015-12-09 15:54:21 -0800

    heroku edits

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
 
 
-app.listen(8080);
+app.listen(port);
 console.log('on 8080');
